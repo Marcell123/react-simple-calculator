e993ea166d03c402cae13d3077e8670059988d66
Artem Dekhanov <zohanaizer2@gmail.com>
2019-06-01 00:42:37 +0300

    Updates

---
+++ precache-manifest.b615cd29d573e33b96b3e59e5ebf692d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c010cc13fb9db7a1572",
+    "url": "/react-simple-calculator/static/js/runtime~main.3c010cc1.js"
+  },
+  {
+    "revision": "c499b17f9c7789665708",
+    "url": "/react-simple-calculator/static/js/main.c499b17f.chunk.js"
+  },
+  {
+    "revision": "134e33aa46d9beeeceff",
+    "url": "/react-simple-calculator/static/js/2.134e33aa.chunk.js"
+  },
+  {
+    "revision": "c499b17f9c7789665708",
+    "url": "/react-simple-calculator/static/css/main.9338182a.chunk.css"
+  },
+  {
+    "revision": "dac062b5019bac23228e6a06d1b1b77a",
+    "url": "/react-simple-calculator/index.html"
+  }
+];
